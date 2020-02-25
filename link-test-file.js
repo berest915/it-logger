d0118362dee28a62bc7156d3b78e0e02718a79e1
elude21933 <43527518+rest-media@users.noreply.github.com>
2020-02-25 20:34:28 +0800

    mod security issue with materialize-css, consider use alternatives for production, And main focus should on Redux feature

---
+++ link-test-file.js
@@ -0,0 +1,15 @@
+//! npm i @server
+//? npm i -D json-server concurrently
+
+//! Full Code Github Repo:
+//? https://github.com/bradtraversy/it-logger
+
+//! JSON-Server Docs & Repo:
+//? https://github.com/typicode/json-server
+
+//! MaterializeCSS Docs:
+//? https://materializecss.com/
+
+// When you are working on another machine and you want to access the json-server via an URL, simply add the IP-Adress of the device like this:
+// json-server --watch db.json --host 192.168.0.xx --port 5000
+
