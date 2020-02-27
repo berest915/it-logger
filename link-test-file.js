1811218ead581f3700756da9210ee80b15045056
elude21933 <43527518+rest-media@users.noreply.github.com>
2020-02-27 20:38:48 +0800

    Initialize Redux Startup/Boiler - Plate

--- link-test-file.js
+++ link-test-file.js
@@ -1,6 +1,11 @@
 //! npm i @server
 //? npm i -D json-server concurrently
 
+//! npm i @client
+//? npm i materialize-css 
+//? npm i moment react-moment
+//? npm i redux react-redux redux-thunk redux-devtools-extension
+
 //! Full Code Github Repo:
 //? https://github.com/bradtraversy/it-logger
 
@@ -9,6 +14,8 @@
 
 //! MaterializeCSS Docs:
 //? https://materializecss.com/
+//? <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 
 // When you are working on another machine and you want to access the json-server via an URL, simply add the IP-Adress of the device like this:
 // json-server --watch db.json --host 192.168.0.xx --port 5000
